8f006a281ab9979dc57c6a36d30055fb296cbbbb
M.D. van Es <es@mdworld.nl>
2020-03-29 19:10:26 +0200

    Support jpg attachments

--- src/formatter/format.ts
+++ src/formatter/format.ts
@@ -20,6 +20,29 @@ function createMessageTemplate(color: string, sender: Sender): string {
   );
 }
 
+const formatAttachment = (message: string) => {
+  if(message.indexOf('file attached') > -1) {
+    // console.log('FILE ATTACHED', message);
+
+    const resultingMessage = message.replace(
+      /(.*)\.(.*) \(file attached\) (.*)/,
+      (match, attachmentName, attachmentExt, restMessage) => {
+        // console.log('FILEMATCH', match, '|', p1, '|', p2);
+        if (attachmentExt === 'mp4') {
+          return `<video src="${attachmentName}.${attachmentExt}" /> ${restMessage}`;
+        } else {
+          return `<img src="${attachmentName}.${attachmentExt}" /> ${restMessage}`;
+        }
+      }
+    );
+
+    // console.log('RESULT', resultingMessage);
+    return resultingMessage;
+  }
+
+  return message;
+};
+
 function formatMessages(
   [currentMessage, ...messages]: ReadonlyArray<WhatsAppMessage>,
   messageTemplates: ReadonlyMap<Sender, string>,
@@ -36,7 +59,7 @@ function formatMessages(
     );
   const resultingMessage = (template as string).replace(
     /{time}(.+){message}/,
-    (match, p1) => currentMessage.time + p1 + currentMessage.message
+    (match, p1) => currentMessage.time + p1 + formatAttachment(currentMessage.message)
   );
 
   if (currentMessage.date !== currentDate) {
